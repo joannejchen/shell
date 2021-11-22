ea9d78b1418d3ceb65f6f542e55ea49178528edb
joannejchen <chenjoanne100k@gmail.com>
2021-11-22 11:55:49 -0600

    parsing

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define block_size 1024
+#define input_size 1024
+#define args_size 64
 
 void turtle_run();
 char* turtle_read();
@@ -31,7 +33,7 @@ void turtle_run() {
         // Parse the string into its command and arguments
         args = turtle_parse(input);
         // Execute the command
-        status = turtle_execute();
+        // status = turtle_execute();
 
         // clean memory for the next command
         free(input);
@@ -41,19 +43,19 @@ void turtle_run() {
 }
 
 char* turtle_read() {
-    int buffer_size = block_size;
+    int buffer_size = input_size;
     int index = 0;
     char* buffer = malloc(sizeof(char) * buffer_size);
     int letter;
 
     // make sure we had a successful malloc
     if (!buffer) {
-        fprintf(stderr, "turtle failed to read");
+        fprintf(stderr, "turtle failed to read\n");
         exit(EXIT_FAILURE);
     }
 
     // read in input from the user until we hit the last character
-    while (true) {
+    while (1) {
         letter = getchar();
 
         // check if this char is the last character
@@ -67,12 +69,12 @@ char* turtle_read() {
 
         // determine whether we need to allocate more space
         if (index >= buffer_size) {
-            buffer_size += block_size;
+            buffer_size += input_size;
             buffer = realloc(buffer, buffer_size);
 
             // make sure we had a successful malloc
             if (!buffer) {
-                fprintf(stderr, "turtle failed to read");
+                fprintf(stderr, "turtle failed to read\n");
                 exit(EXIT_FAILURE);
             }
         }
@@ -81,9 +83,44 @@ char* turtle_read() {
 }
 
 char** turtle_parse(char* input) {
-    
+    int buffer_args = args_size;
+    int arg_index = 0;
+    char** args = malloc(sizeof(char*) * buffer_args);
+    char* cur_arg;
+
+    // determine whether the malloc failed
+    if (!args) {
+        fprintf(stderr, "turtle failed to parse\n");
+        exit(EXIT_FAILURE);
+    }
+
+    cur_arg = strtok(input, " \t\r\n\a");
+    while (cur_arg != NULL) {
+        args[arg_index] = cur_arg;
+        arg_index++;
+
+        if (arg_index >= buffer_args) {
+            buffer_args += args_size;
+            args = realloc(args, buffer_args * sizeof(char*));
+            if (!args) {
+                fprintf(stderr, "turtle failed to parse\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        cur_arg = strtok(NULL, " \t\r\n\a");
+    }
+
+    args[arg_index] = NULL;
+
+    // // ensure proper parsing
+    // for (int i = 0; i < arg_index; i++) {
+    //     printf("%s\n", args[i]);
+    // }
+
+    // return args;
 }
 
 int turtle_execute(char** args) {
-    
+    return 0;
 }
